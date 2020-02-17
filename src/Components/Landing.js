ed2b7da9d2b40411aef3a8b0d4a2a7c006c37360
JaredH <jaredezzethasson@gmail.com>
2020-02-17 11:37:54 -0700

    changes to landing component

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFacebook, faGithub, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons";
-import {faAngleDown} from "@fortawesome/free-solid-svg-icons";
+import {faAngleDown, faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-scroll";
 import tedxname from "../Assets/TED Youth Logo.jpg"
+import doterra from "../Assets/doterra-wellness-advocate-white.png"
 
 class Landing extends Component {
     constructor(props){
@@ -14,11 +15,17 @@ class Landing extends Component {
         return (
             <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
                 <div className="w-100">
-                    <img width={1000} src={tedxname} alt="TEDx PleasantGroveBlvd"/>
-                    <div className="subheading mb-5">{this.landingData.phoneNumber} ·
-                        <a href={"mailto:" + this.landingData.email}>{"  " + this.landingData.email}</a>
-                    </div>
-                    <p className="lead mb-5">{this.landingData.bio1}</p>
+                    <img src={tedxname} style={{maxWidth: "100%"}} alt="TEDx PleasantGroveBlvd"/>
+                    <a style={{textDecoration: 'none'}} href={this.landingData.doterraDirections}>
+                        <div className="pl-4 mt-0">
+                            <p style={{color: "white", fontSize: "20px"}}>
+                                <FontAwesomeIcon icon={faMapMarkerAlt} color={"white"} className="mr-1"/> Hosted by <img src={doterra} alt={"doterra logo"} height={15}/>
+                            </p>
+                        </div>
+                    </a>
+                    <h4 className="pt-5 pl-4" style={{color: "white"}}>What is TEDx?</h4>
+                    <p className="lead pl-4 mt-3">{this.landingData.tedxplanation}<br/>{this.landingData.tedxplanation2}</p>
+                    <p className="lead pl-4 mb-5">{}</p>
                     <div className="social-icons">
                         <a href={this.landingData.linkedin}>
                             <FontAwesomeIcon icon={faLinkedin} />
